ca97e91b016107d85ebc6af3a4106518b7ea386d
Hyun <87049207+vaynel@users.noreply.github.com>
2023-08-02 10:40:42 +0900

    setting

---
+++ src/main/java/com/solo/LoLStrategy/LoLStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.solo.LoLStrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solo/LoLStrategy/LoLStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.LoLStrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoLStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
