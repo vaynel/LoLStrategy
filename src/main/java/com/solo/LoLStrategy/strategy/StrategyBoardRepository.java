c9449765e7ce1bfe502aea527552155d6cd42860
Hyun <87049207+vaynel@users.noreply.github.com>
2023-08-30 01:31:37 +0900

    board page

--- src/main/java/com/solo/LoLStrategy/strategy/StrategyBoardRepository.java
+++ src/main/java/com/solo/LoLStrategy/strategy/StrategyBoardRepository.java
@@ -1,7 +1,6 @@
 package com.solo.LoLStrategy.strategy;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
